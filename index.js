1d3e65b2d6dae3b9c852b6149d02db413b8abc80
david.garcia <david.garcia@beeva.com>
2017-09-27 17:44:46 +0200

    Node version control

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 'use strict';
 
+var version = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
+if(version < 8) {
+  console.log('Spiderhack requires node v.8+');
+  console.log('Install with Node Version Manager (NVM)');
+  console.log('https://github.com/creationix/nvm');
+  console.log('');
+  process.exit(1);
+}
+
+
 const SpiderHack = require('./lib/SpiderHack');
 const s = new SpiderHack();
 s.init();
